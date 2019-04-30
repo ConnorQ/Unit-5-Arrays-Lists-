8dfd5bc7a005727abd7b663284556301cb90afc7
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-04-30 12:26:22 -0400

    max value

---
+++ FindMaxValue/FindMaxValue/FindMaxValueForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace FindMaxValue
+{
+    partial class frmFindMaxValue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.lblMaxValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(161, 29);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.ItemHeight = 16;
+            this.lstNumbers.Location = new System.Drawing.Point(62, 76);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(278, 148);
+            this.lstNumbers.TabIndex = 1;
+            // 
+            // lblMaxValue
+            // 
+            this.lblMaxValue.AutoSize = true;
+            this.lblMaxValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaxValue.Location = new System.Drawing.Point(123, 273);
+            this.lblMaxValue.Name = "lblMaxValue";
+            this.lblMaxValue.Size = new System.Drawing.Size(15, 16);
+            this.lblMaxValue.TabIndex = 2;
+            this.lblMaxValue.Text = "?";
+            // 
+            // frmFindMaxValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 326);
+            this.Controls.Add(this.lblMaxValue);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmFindMaxValue";
+            this.Text = "Find Max Value by Connor Quinlan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Label lblMaxValue;
+    }
+}
+
---
+++ FindMaxValue/FindMaxValue/FindMaxValueForm.cs
@@ -0,0 +1,86 @@
+﻿/*
+ * Created by: Connor Quinlan
+ * Created on: 04-30-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #36 - Find Max Value
+ * This program finds the max value of a group of integers.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindMaxValue
+{
+    public partial class frmFindMaxValue : Form
+    {
+        public frmFindMaxValue()
+        {
+            InitializeComponent();
+        }
+
+        private int GetMaxValue(int[] tmpArrayOfIntegers)
+        {
+            //declaring variables and constants
+            int tmpMaxValue = 0;
+            int counter;
+            const int MAX_ARRAY_SIZE = 10;
+
+
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+
+                if (tmpArrayOfIntegers[counter] > tmpMaxValue)
+                {
+                    //Sets the current max value to the current index if it is greater than the last max value
+                    tmpMaxValue = tmpArrayOfIntegers[counter];
+                }
+            }
+
+            //return the max value 
+            return tmpMaxValue;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // declare local variables and constants
+            const int MAX_ARRAY_SIZE = 10;
+            const int MAX_RANDOM_NUMBER = 500;
+            int[] arrayOfIntegers = new int[MAX_ARRAY_SIZE];
+            int counter, randomNumber, maxValue;
+            Random randomNumberGenerator = new Random();
+
+            //Clears the listbox
+            lstNumbers.Items.Clear();
+
+
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+                //generate a random number
+                randomNumber = randomNumberGenerator.Next(1, MAX_RANDOM_NUMBER + 1);
+
+                //Setting the current index of the array to the generated number
+                arrayOfIntegers[counter] = randomNumber;
+
+                //Add the number to the listbox
+                this.lstNumbers.Items.Add(randomNumber);
+
+                //refresh the form
+                this.Refresh();
+
+            }
+
+            //call the GetMaxValue function
+            maxValue = GetMaxValue(arrayOfIntegers);
+
+            //Display the max value
+            lblMaxValue.Text = "The max value is: " + maxValue;
+        }
+    }
+}

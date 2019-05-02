c9737c9f453d39e21ccdaf6f7fb6da7ae41faad9
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-05-02 12:45:35 -0400

    min

---
+++ FindMinValueConnorQ/FindMinValueConnorQ/FindMinValueForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace FindMinValueConnorQ
+{
+    partial class frmFindMinValue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.ItemHeight = 16;
+            this.lstNumbers.Location = new System.Drawing.Point(130, 109);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(279, 228);
+            this.lstNumbers.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(240, 38);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(167, 368);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(15, 16);
+            this.lblAnswer.TabIndex = 3;
+            this.lblAnswer.Text = "?";
+            // 
+            // frmFindMinValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 430);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lstNumbers);
+            this.Name = "frmFindMinValue";
+            this.Text = "Find Min Value by Connor Quinlan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ FindMinValueConnorQ/FindMinValueConnorQ/FindMinValueForm.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Created by: Connor Quinlan
+ * Created on: 02-05-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #37 - Find Min Value
+ * This program that generates a group of numbers and it finds the smallest one.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindMinValueConnorQ
+{
+    public partial class frmFindMinValue : Form
+    {
+
+        // declare my global variables and constants
+        const int MAX_ARRAY_SIZE = 10;
+        const int MAX_RANDOM_NUMBER = 500;
+        int[] arrayOfNumbers = new int[MAX_ARRAY_SIZE];
+
+        public frmFindMinValue()
+        {
+            InitializeComponent();
+        }
+
+        private int GetMaxValue(int[] arrayOfNumbers)
+        {
+            // declare local variables
+            int tmpMinValue = 500;
+            int counter = 0;
+
+
+            // calculate the the min value in the list box
+            foreach (double arrayValue in arrayOfNumbers)
+            {
+                if (arrayOfNumbers[counter] < tmpMinValue)
+                {
+                    tmpMinValue = arrayOfNumbers[counter];
+
+                }
+                counter++;
+            }
+            return tmpMinValue;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // create my variables
+            int randomNumber, counter, minValue;
+            Random randomNumberGenerator = new Random();
+
+            // clear the list box
+            this.lstNumbers.Items.Clear();
+
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+                // generate a new random number beetween 1 and the MAX_ARRAY_SIZE
+                randomNumber = randomNumberGenerator.Next(1, MAX_RANDOM_NUMBER);
+
+                // assign the random number to the cell position "counter" in the array
+                arrayOfNumbers[counter] = randomNumber;
+
+                // add the random number to the list
+                this.lstNumbers.Items.Add(randomNumber);
+
+                // call the function to calculate the max value
+                minValue = GetMaxValue(arrayOfNumbers);
+
+                // display the min value
+                lblAnswer.Text = "The min value is " + minValue;
+            }
+        }
+    }
+}

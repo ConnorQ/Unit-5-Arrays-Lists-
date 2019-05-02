8f6b658e6279c1025fdaf4e810206505431fb85b
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-05-02 18:19:20 -0400

    read write

---
+++ ReadWrite/StringEqualityConnorQ/StringEqualityConnorQ/StringEqualityForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace StringEqualityConnorQ
+{
+    partial class frmStringEquality
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(144, 125);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(47, 66);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(267, 16);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "Output has been displayed in new output file";
+            // 
+            // frmStringEquality
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 288);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "frmStringEquality";
+            this.Text = "String Equality by Connor Quinlan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ ReadWrite/StringEqualityConnorQ/StringEqualityConnorQ/StringEqualityForm.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Created by: Connor Quinlan
+ * Created on: 30-04-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #40 - Read Write
+ * This program tests if pairs of strings are equal.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringEqualityConnorQ
+{
+    public partial class frmStringEquality : Form
+    {
+        public frmStringEquality()
+        {
+            InitializeComponent();
+        }
+
+        public bool Check(string input1, string input2)
+        {
+            //set the strings to not equal
+            bool equal = false;
+
+            //check if the strings are the same length
+            if (input1.Length == input2.Length)
+            {
+                //set them both to all capitals
+                input1 = input1.ToUpper();
+                input2 = input2.ToUpper();
+
+                //check if the strings are the same
+                if (input1 == input2)
+                {
+                    equal = true;
+                }
+            }
+            return equal;
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            // copy input file to lines array
+            string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+
+            // array of spaces to know how to split words
+            char[] charSeparators = new char[] { ' ', '\t' };
+
+            // string used for results
+            string output = "";
+
+            // go through and compare each string on each line of the file
+            foreach (string line in lines)
+            {
+                // split up words on line\
+                string[] words = line.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+                if (Check(words[0], words[1]) == true)
+                {
+                    // add true to output
+                    output = output + "True\r\n";
+                }
+                else
+                {
+                    // add false to output
+                    output = output + "False\r\n";
+                }
+
+                // create output file and display the output on it
+                System.IO.File.WriteAllText(@"output.txt", output);
+            }
+        }
+    }
+}
